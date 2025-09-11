aaea5e8fbc1568dabbe5656daf4ee1cc1aeb33f1
Azizbek <azizbekxurramov0102@mail.com>
2025-09-11 09:11:08 +0500

    teskarisi qushildi

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        long katta = 45689;
+        long teskari=0;
+        while (katta!=0){
+            teskari=teskari*10+(katta%10);
+            katta=katta/10;
         }
+        System.out.println(teskari);
 
-        salom();
-        salom2();
-        qushish(2,3);
-        bir();
+
     }
 
     private static void bir() {
